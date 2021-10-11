a281b120e286d9e210bdb775663d46cea6adf941
LeoSouza99 <leocsf99@gmail.com>
2021-10-11 01:08:14 -0300

    Criando classe DisponibilidadeTurno

---
+++ src/main/java/com/leosouza/sge/domain/DisponibilidadeDia.java
@@ -0,0 +1,63 @@
+package com.leosouza.sge.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class DisponibilidadeDia {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String dia;
+    private Boolean ativo;
+
+    public DisponibilidadeDia() {
+    }
+
+    public DisponibilidadeDia(Long id, String dia, Boolean ativo) {
+        this.id = id;
+        this.dia = dia;
+        this.ativo = ativo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDia() {
+        return dia;
+    }
+
+    public void setDia(String dia) {
+        this.dia = dia;
+    }
+
+    public Boolean getAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DisponibilidadeDia that = (DisponibilidadeDia) o;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
